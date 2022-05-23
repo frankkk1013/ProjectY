f51b5d49c396e94e1c217b446f637f7eb6de373f
frankkk1013 <59925828+frankkk1013@users.noreply.github.com>
2022-05-23 10:07:36 +0200

    creation

---
+++ ProjectY/ProjectY/ProjectYApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectYApp.swift
+//  ProjectY
+//
+//  Created by Francesco Iaccarino on 23/05/22.
+//
+
+import SwiftUI
+
+@main
+struct ProjectYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
