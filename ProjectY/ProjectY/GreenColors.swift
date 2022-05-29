91a390466dfc25822ded65efb9a34fea26af329f
boscus00 <boscus.now2000@gmail.com>
2022-05-30 00:12:20 +0200

    LeavesCollectedView

---
+++ ProjectY/ProjectY/GreenColors.swift
@@ -0,0 +1,15 @@
+//
+//  GreenColors.swift
+//  ProjectY
+//
+//  Created by Emanuele Bosco on 29/05/22.
+//
+
+import SwiftUI
+
+extension Color{
+    
+    public static let myColor: Color = Color(UIColor(red:255/255, green:255/255, blue:255/255, alpha: 1.))
+    
+    
+}
