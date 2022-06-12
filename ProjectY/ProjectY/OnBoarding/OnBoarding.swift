e417fe3a5b1dad91f8a55f70310ec68432186801
frankkk1013 <59925828+frankkk1013@users.noreply.github.com>
2022-06-12 19:56:23 +0200

    onboarding corretta

--- ProjectY/ProjectY/OnBoarding/OnBoarding.swift
+++ ProjectY/ProjectY/OnBoarding/OnBoarding.swift
@@ -18,45 +18,63 @@ struct OnBoarding: View {
         
         ]
     var body: some View {
-        VStack {
-            Spacer()
-                Text("What's this")
-                    .fontWeight(.heavy)
-                    .font(.system(size: 50))
-                    .frame(width: 300, alignment: .leading)
         
-                
-                VStack(alignment: .leading) {
-                
-                        NewDetail(image: "luggage", imageColor: Color("INNER PARTY"), title: "Powers", description: "Pack your suitcase in a sustainable way")
-                        NewDetail(image: "list", imageColor: Color.orange, title: "Journey", description: "Check the lists to not forget anything")
-                        NewDetail(image: "bucket", imageColor: Color("EXAWER"), title: "Sounds", description: "Delete the unsustainable items")
-                        NewDetail(image: "leaf", imageColor: Color("EXAWER"), title: "Sounds", description: "Earn leaves and see how sustainable you are")
-                        
+        ZStack{
+            Rectangle().foregroundColor(Color("onboarding")).edgesIgnoringSafeArea(.all)
+            PlayerView()
+                           
+                            .blur(radius: 0)
+                            .shadow(radius: 13)
+//                            .scaledToFit()
+                            
+                            .frame(width: 350, height: 500, alignment: .center)
+            VStack{
+                Spacer()
+                Button(action: {
+                    UserDefaults.standard.set(true, forKey: "LaunchBefore")
+                    withAnimation(){
+                        needsAppOnboarding = false
+                        presentationMode.wrappedValue.dismiss()
+                       
+                    }
+                }){
+                Text("Next")
+                    .foregroundColor(.white)
+                    .font(.headline)
+                    .frame(width: 320, height: 60)
+                    .background(Color(#colorLiteral(red: 0.4032904506, green: 0.7195885181, blue: 0.4985763431, alpha: 1)))
+                    .cornerRadius(15)
+                    .padding(.top, 50)
                     
-                    
+                }.padding()
             }
-
-            Spacer()
             
-            Button(action: {
-                UserDefaults.standard.set(true, forKey: "LaunchBefore")
-                withAnimation(){
-                    needsAppOnboarding = false
-                    presentationMode.wrappedValue.dismiss()
-                   
-                }
-            }){
-            Text("Next")
-                .foregroundColor(.white)
-                .font(.headline)
-                .frame(width: 320, height: 60)
-                .background(Color(#colorLiteral(red: 0.4032904506, green: 0.7195885181, blue: 0.4985763431, alpha: 1)))
-                .cornerRadius(15)
-                .padding(.top, 50)
-                
-            }.padding()
+            
         }
+        
+//        VStack {
+//            Spacer()
+//                Text("What's this")
+//                    .fontWeight(.heavy)
+//                    .font(.system(size: 50))
+//                    .frame(width: 300, alignment: .leading)
+//        
+//                
+//                VStack(alignment: .leading) {
+//                
+//                        NewDetail(image: "luggage", imageColor: Color("INNER PARTY"), title: "Powers", description: "Pack your suitcase in a sustainable way")
+//                        NewDetail(image: "list", imageColor: Color.orange, title: "Journey", description: "Check the lists to not forget anything")
+//                        NewDetail(image: "bucket", imageColor: Color("EXAWER"), title: "Sounds", description: "Delete the unsustainable items")
+//                        NewDetail(image: "leaf", imageColor: Color("EXAWER"), title: "Sounds", description: "Earn leaves and see how sustainable you are")
+//                        
+//                    
+//                    
+//            }
+//
+//            Spacer()
+//            
+            
+//        }
     }
 }
 
